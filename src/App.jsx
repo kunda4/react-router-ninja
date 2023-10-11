be4864838e9d881a2fceb8e70d8478c20418979e
Agnes IRADUKUNDA <101483232+kunda4@users.noreply.github.com>
2023-10-11 10:49:42 +0200

    Help Layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import { createBrowserRouter,  Route, createRoutesFromElements, RouterProvider }
 import About from "./pages/About"
 import Home from "./pages/Home"
 import LootLayout from "./layouts/LootLayout"
+import HelpLayout from "./layouts/HelpLayout"
 function App() {
 
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element= {<LootLayout/>}>
-      <Route index element={<Home/>} />
-      <Route path="/about" element={<About/>} />
+        <Route index element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="help" element={<HelpLayout/>}>
+          <Route path="faq"/>
+          <Route path="contact"/>
+        </Route>
     </Route>
     )
   )
