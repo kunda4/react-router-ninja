4e6d24450179e793887638ad4afb6d658c6e00ca
Agnes IRADUKUNDA <101483232+kunda4@users.noreply.github.com>
2023-10-11 10:53:21 +0200

    FAQ page

---
+++ src/pages/help/Faq.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+export default function Faq() {
+  return (
+    <div className="faq">
+        <h3>Flequently Asked Question</h3>
+
+        <div className="question">
+            <p><strong>Lorem ipsum dolor sit, amet consectetur adipisicing elit</strong></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil eaque 
+                quasi et ducimus praesentium non nam consequatur repellat eveniet commodi 
+                ratione, nulla obcaecati esse consectetur, deserunt maiores adipisci culpa 
+                blanditiis.</p>
+        </div>
+        <div className="question">
+            <p><strong>Lorem ipsum dolor sit, amet consectetur adipisicing elit</strong></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil eaque 
+                quasi et ducimus praesentium non nam consequatur repellat eveniet commodi 
+                ratione, nulla obcaecati esse consectetur, deserunt maiores adipisci culpa 
+                blanditiis.</p>
+        </div>
+        <div className="question">
+            <p><strong>Lorem ipsum dolor sit, amet consectetur adipisicing elit</strong></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil eaque 
+                quasi et ducimus praesentium non nam consequatur repellat eveniet commodi 
+                ratione, nulla obcaecati esse consectetur, deserunt maiores adipisci culpa 
+                blanditiis.</p>
+        </div>
+        <div className="question">
+            <p><strong>Lorem ipsum dolor sit, amet consectetur adipisicing elit</strong></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil eaque 
+                quasi et ducimus praesentium non nam consequatur repellat eveniet commodi 
+                ratione, nulla obcaecati esse consectetur, deserunt maiores adipisci culpa 
+                blanditiis.</p>
+        </div>
+        <div className="question">
+            <p><strong>Lorem ipsum dolor sit, amet consectetur adipisicing elit</strong></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil eaque 
+                quasi et ducimus praesentium non nam consequatur repellat eveniet commodi 
+                ratione, nulla obcaecati esse consectetur, deserunt maiores adipisci culpa 
+                blanditiis.</p>
+        </div>
+    </div>
+  )
+}
